c05e1f600f6911de3ac7d89a89fcafe778510970
srz_zumix <zumix.cpp@gmail.com>
2020-07-01 01:42:17 +0900

    update

--- ci-env.sh
+++ ci-env.sh
@@ -2,7 +2,7 @@
 # ALL Environment Variables
 
 # CUR_DIR=$(cd $(dirname $0) && pwd)
-CUR_DIR=$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")" || cd "$(dirname $0)"; pwd)
 
 . ${CUR_DIR}/ci-env-git-commit.sh
 . ${CUR_DIR}/ci-env-name.sh
