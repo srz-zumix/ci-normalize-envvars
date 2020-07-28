665c19aacf55d39827cad72b2b18b06d996560a3
srz_zumix <zumix.cpp@gmail.com>
2020-07-29 08:28:46 +0900

    fix tag false

--- parts/ci_env_git_tag.sh
+++ parts/ci_env_git_tag.sh
@@ -12,6 +12,8 @@ if [ -n "${APPVEYOR+x}" ]; then
     if [ -n "${APPVEYOR_REPO_TAG_NAME+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${APPVEYOR_REPO_TAG_NAME}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -20,6 +22,8 @@ if [ -n "${AZURE_HTTP_USER_AGENT+x}" ]; then
     if echo ${BUILD_SOURCEBRANCH} | grep -q refs/tags/; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME=$(echo ${BUILD_SOURCEBRANCH} | sed -e s@refs/.*/@@g)
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -28,6 +32,8 @@ if [ -n "${BITRISE_IO+x}" ]; then
     if [ -n "${BITRISE_GIT_TAG+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${BITRISE_GIT_TAG}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -36,6 +42,8 @@ if [ -n "${BUDDY+x}" ]; then
     if [ -n "${BUDDY_EXECUTION_TAG+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${BUDDY_EXECUTION_TAG}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -44,6 +52,8 @@ if [ -n "${CIRCLECI+x}" ]; then
     if [ -n "${CIRCLE_TAG+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${CIRCLE_TAG}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -52,6 +62,8 @@ if [ -n "${CIRRUS_CI+x}" ]; then
     if [ -n "${CIRRUS_TAG+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${CIRRUS_TAG}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -60,6 +72,8 @@ if [ -n "${CF_BUILD_URL+x}" ]; then
     # if [ -n "${CIRRUS_TAG+x}" ]; then
     #     export CI_ENV_GIT_TAG=true
     #     export CI_ENV_GIT_TAG_NAME="${CIRRUS_TAG}"
+    # else
+    #     export CI_ENV_GIT_TAG=false
     # fi
     return
 fi
@@ -69,6 +83,8 @@ if [ -n "${CI_NAME+x}" ]; then
         # if [ -n "${CIRRUS_TAG+x}" ]; then
         #     export CI_ENV_GIT_TAG=true
         #     export CI_ENV_GIT_TAG_NAME="${CIRRUS_TAG}"
+        # else
+        #     export CI_ENV_GIT_TAG=false
         # fi
         return
     fi
@@ -78,6 +94,8 @@ if [ -n "${DRONE+x}" ]; then
     if [ -n "${DRONE_TAG+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${DRONE_TAG}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -86,6 +104,8 @@ if [ -n "${GITHUB_ACTIONS+x}" ]; then
     if echo ${GITHUB_REF} | grep -q refs/tags/; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME=$(echo ${GITHUB_REF} | sed -e s@refs/.*/@@g)
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -95,6 +115,8 @@ if [ -n "${BUILD_URL+x}" ]; then
         # if [ -n "${DRONE_TAG+x}" ]; then
         #     export CI_ENV_GIT_TAG=true
         #     export CI_ENV_GIT_TAG_NAME="${DRONE_TAG}"
+        # else
+        #     export CI_ENV_GIT_TAG=false
         # fi
         return
     fi
@@ -105,6 +127,8 @@ if [ -n "${CI+x}" ]; then
         if [ -n "${CI_TAG+x}" ]; then
             export CI_ENV_GIT_TAG=true
             export CI_ENV_GIT_TAG_NAME="${CI_TAG}"
+        else
+            export CI_ENV_GIT_TAG=false
         fi
         return
     fi
@@ -114,6 +138,8 @@ if [ -n "${SCRUTINIZER+x}" ]; then
     if [ -n "${CI_TAG+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${CI_TAG}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -122,6 +148,8 @@ if [ -n "${SEMAPHORE+x}" ]; then
     if [ -n "${SEMAPHORE_GIT_TAG_NAME+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${SEMAPHORE_GIT_TAG_NAME}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -130,6 +158,8 @@ if [ -n "${SHIPPABLE+x}" ]; then
     if [ -n "${GIT_TAG_NAME+x}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${GIT_TAG_NAME}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -138,6 +168,8 @@ if [ -n "${TRAVIS+x}" ]; then
     if [ -n "${TRAVIS_TAG}" ]; then
         export CI_ENV_GIT_TAG=true
         export CI_ENV_GIT_TAG_NAME="${TRAVIS_TAG}"
+    else
+        export CI_ENV_GIT_TAG=false
     fi
     return
 fi
@@ -146,6 +178,8 @@ if [ -n "${WERCKER_RUN_URL+x}" ]; then
     # if [ -n "${TRAVIS_TAG+x}" ]; then
     #     export CI_ENV_GIT_TAG=true
     #     export CI_ENV_GIT_TAG_NAME="${TRAVIS_TAG}"
+    # else
+    #     export CI_ENV_GIT_TAG=false
     # fi
     return
 fi
