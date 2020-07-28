e123d74f563f9f42aeab29ca225fcc6894d09651
srz_zumix <zumix.cpp@gmail.com>
2020-07-29 08:48:56 +0900

    fix envs

--- ci-envs.sh
+++ ci-envs.sh
@@ -3,6 +3,6 @@
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)
 
-for file in `find . -name *.sh -maxdepth 1 -type f`; do
+for file in `find ${CUR_DIR} -name \*.sh -maxdepth 1 -type f`; do
     . ${file}
 done
