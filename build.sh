70dc7a7c970c8a1b4592d59ced98897201b9196c
srz_zumix <zumix.cpp@gmail.com>
2021-09-15 11:23:18 +0900

    fix shellcheck

--- build.sh
+++ build.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)
-OUTPUT=${CUR_DIR}/ci-env.sh
+OUTPUT="${CUR_DIR}/ci-env.sh"
 
-echo "#!/bin/bash" > ${OUTPUT}
-echo "# ALL Environment Variables" >> ${OUTPUT}
-echo "" >> ${OUTPUT}
+{
+    echo "#!/bin/bash"
+    echo "# ALL Environment Variables"
+    echo ""
+} > "${OUTPUT}"
 
-for file in `find ${CUR_DIR}/parts -name \*.sh -maxdepth 1 -type f | sort`; do
-    BASENAME=$(basename ${file})
-    echo ${BASENAME}
-    NAME=${BASENAME%%.sh}
-    echo "${NAME}() {" >> ${OUTPUT}
-    sed -e "1d" ${file} >> ${OUTPUT}
-    echo "}" >> ${OUTPUT}
-    echo "" >> ${OUTPUT}
+for file in `find "${CUR_DIR}/parts" -name \*.sh -maxdepth 1 -type f | sort`; do
+    BASENAME=$(basename "${file}")
+    echo "${BASENAME}"
+    NAME="${BASENAME%%.sh}"
+    {
+        echo "${NAME}() {"
+        sed -e "1d" "${file}"
+        echo "}"
+        echo "" 
+    } >> "${OUTPUT}"
 done
 
-for file in `find ${CUR_DIR}/parts -name \*.sh -maxdepth 1 -type f | sort`; do
-    BASENAME=$(basename ${file})
-    NAME=${BASENAME%%.sh}
-    echo ${NAME} >> ${OUTPUT}    
+for file in `find "${CUR_DIR}/parts" -name \*.sh -maxdepth 1 -type f | sort`; do
+    BASENAME=$(basename "${file}")
+    NAME="${BASENAME%%.sh}"
+    echo "${NAME}" >> "${OUTPUT}" 
 done
 
-echo export CI=true >> ${OUTPUT}
+echo export CI=true >> "${OUTPUT}"
