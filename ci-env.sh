9844b11a8263fb98eab64f3069cfc7f12a946746
srz-zumix <noreply@github.com>
2022-07-02 05:04:41 +0000

    update all in one [ci skip]

--- ci-env.sh
+++ ci-env.sh
@@ -418,99 +418,104 @@ fi
 ci_env_git_commit() {
 # CI_ENV_GIT_COMMIT
 
-if [ -n "${CI_ENV_GIT_COMMIT+x}" ]; then
-    return
-fi
+detect_git_commit() {
+    if [ -n "${CI_ENV_GIT_COMMIT+x}" ]; then
+        return
+    fi
 
-if [ -n "${AC_GIT_COMMIT+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${AC_GIT_COMMIT}"
-    return
-fi
+    if [ -n "${AC_GIT_COMMIT+x}" ]; then
+        CI_ENV_GIT_COMMIT="${AC_GIT_COMMIT}"
+        return
+    fi
 
-if [ -n "${APPVEYOR_REPO_COMMIT+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${APPVEYOR_REPO_COMMIT}"
-    return
-fi
+    if [ -n "${APPVEYOR_REPO_COMMIT+x}" ]; then
+        CI_ENV_GIT_COMMIT="${APPVEYOR_REPO_COMMIT}"
+        return
+    fi
 
-if [ -n "${BUILD_SOURCEVERSION+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${BUILD_SOURCEVERSION}"
-    return
-fi
+    if [ -n "${BUILD_SOURCEVERSION+x}" ]; then
+        CI_ENV_GIT_COMMIT="${BUILD_SOURCEVERSION}"
+        return
+    fi
 
-if [ -n "${BITRISE_CI_ENV_GIT_COMMIT+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${BITRISE_CI_ENV_GIT_COMMIT}"
-    return
-fi
+    if [ -n "${BITRISE_CI_ENV_GIT_COMMIT+x}" ]; then
+        CI_ENV_GIT_COMMIT="${BITRISE_CI_ENV_GIT_COMMIT}"
+        return
+    fi
 
-if [ -n "${BUDDY_EXECUTION_REVISION+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${BUDDY_EXECUTION_REVISION}"
-    return
-fi
+    if [ -n "${BUDDY_EXECUTION_REVISION+x}" ]; then
+        CI_ENV_GIT_COMMIT="${BUDDY_EXECUTION_REVISION}"
+        return
+    fi
 
-if [ -n "${CIRCLE_SHA1+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${CIRCLE_SHA1}"
-    return
-fi
+    if [ -n "${CIRCLE_SHA1+x}" ]; then
+        CI_ENV_GIT_COMMIT="${CIRCLE_SHA1}"
+        return
+    fi
 
-if [ -n "${CIRRUS_CHANGE_IN_REPO+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${CIRRUS_CHANGE_IN_REPO}"
-    return
-fi
+    if [ -n "${CIRRUS_CHANGE_IN_REPO+x}" ]; then
+        CI_ENV_GIT_COMMIT="${CIRRUS_CHANGE_IN_REPO}"
+        return
+    fi
 
-if [ -n "${CF_REVISION+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${CF_REVISION}"
-    return
-fi
+    if [ -n "${CF_REVISION+x}" ]; then
+        CI_ENV_GIT_COMMIT="${CF_REVISION}"
+        return
+    fi
 
-if [ -n "${CI_COMMIT_ID+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${CI_COMMIT_ID}"
-    return
-fi
+    if [ -n "${CI_COMMIT_ID+x}" ]; then
+        CI_ENV_GIT_COMMIT="${CI_COMMIT_ID}"
+        return
+    fi
 
-if [ -n "${DRONE_COMMIT_SHA+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${DRONE_COMMIT_SHA}"
-    return
-fi
+    if [ -n "${DRONE_COMMIT_SHA+x}" ]; then
+        CI_ENV_GIT_COMMIT="${DRONE_COMMIT_SHA}"
+        return
+    fi
 
-if [ -n "${GITHUB_SHA+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${GITHUB_SHA}"
-    return
-fi
+    if [ -n "${GITHUB_SHA+x}" ]; then
+        CI_ENV_GIT_COMMIT="${GITHUB_SHA}"
+        return
+    fi
 
-# JFrog Pipelines
-if [ -n "${JFROG_CLI_BUILD_NAME+x}" ]; then
-    export CI_ENV_GIT_COMMIT=$(env | grep "res.*_commitSha" | head -1 | sed "s/.*=//")
-    return
-fi
+    # JFrog Pipelines
+    if [ -n "${JFROG_CLI_BUILD_NAME+x}" ]; then
+        CI_ENV_GIT_COMMIT=$(env | grep "res.*_commitSha" | head -1 | sed "s/.*=//")
+        return
+    fi
 
-# RazorOps
-if [ -n "${COMMIT+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${COMMIT}"
-    return
-fi
+    # RazorOps
+    if [ -n "${COMMIT+x}" ]; then
+        CI_ENV_GIT_COMMIT="${COMMIT}"
+        return
+    fi
 
-if [ -n "${SCRUTINIZER_SHA1+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${SCRUTINIZER_SHA1}"
-    return
-fi
+    if [ -n "${SCRUTINIZER_SHA1+x}" ]; then
+        CI_ENV_GIT_COMMIT="${SCRUTINIZER_SHA1}"
+        return
+    fi
 
-if [ -n "${SEMAPHORE_GIT_SHA+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${SEMAPHORE_GIT_SHA}"
-    return
-fi
+    if [ -n "${SEMAPHORE_GIT_SHA+x}" ]; then
+        CI_ENV_GIT_COMMIT="${SEMAPHORE_GIT_SHA}"
+        return
+    fi
 
-if [ -n "${TRAVIS_COMMIT+x}" ]; then
-    export CI_ENV_GIT_COMMIT="${TRAVIS_COMMIT}"
-    return
-fi
+    if [ -n "${TRAVIS_COMMIT+x}" ]; then
+        CI_ENV_GIT_COMMIT="${TRAVIS_COMMIT}"
+        return
+    fi
 
 
-if [ -z "${CI_ENV_GIT_COMMIT+x}" ]; then
-    export CI_ENV_GIT_COMMIT=$(git rev-parse HEAD)
-fi
-if [ -z "${CI_ENV_GIT_COMMIT+x}" ]; then
-    export CI_ENV_CI_ENV_GIT_COMMIT=$1
-fi
+    if [ -z "${CI_ENV_GIT_COMMIT+x}" ]; then
+        CI_ENV_GIT_COMMIT=$(git rev-parse HEAD)
+    fi
+    if [ -z "${CI_ENV_GIT_COMMIT+x}" ]; then
+        CI_ENV_CI_ENV_GIT_COMMIT="${1:-}"
+    fi
+}
+
+detect_git_commit
+export CI_ENV_CI_ENV_GIT_COMMIT
 }
 
 ci_env_git_tag() {
